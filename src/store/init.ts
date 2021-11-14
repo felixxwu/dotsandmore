e65a1b07294c63b674203a4ced5b4abc44d93df2
Felix Wu <felix@2simple.com>
2021-11-14 16:10:38 +0000

    Big boy typescript refactor

--- src/store/init.ts
+++ src/store/init.ts
@@ -1,75 +1,51 @@
 import {Coord, LineType} from '@/types'
 
-const storeState = {
+const config = {
     // lengths
-    appWidth: 0,
-    appHeight: 0,
-    paddingMed: 10,
-    cellWidth: 30,
-    dotSize: 4,
-    shadowBlurRadius: 0,
+    appWidth:                   {value: 0,                                       unit: 'px'},
+    appHeight:                  {value: 0,                                       unit: 'px'},
+    paddingMed:                 {value: 10,                                      unit: 'px'},
+    cellWidth:                  {value: 30,                                      unit: 'px'},
+    dotSize:                    {value: 4,                                       unit: 'px'},
+    shadowBlurRadius:           {value: 0,                                       unit: 'px'},
 
     // numbers
-    gridSizeW: 0,
-    gridSizeH: 0,
-    maxLineLength: 2.5,
-    mouseThrottle: 20,
-    minDistanceToLineForShadow: 1,
-    shadowOpacity: 0.03,
-    intersectionTolerance: 0.0001,
-    populateLinesDuration: 5000,
+    gridSizeW:                  {value: 0,                                       unit: ''},
+    gridSizeH:                  {value: 0,                                       unit: ''},
+    maxLineLength:              {value: 2.5,                                     unit: ''},
+    mouseThrottle:              {value: 20,                                      unit: ''},
+    minDistanceToLineForShadow: {value: 1,                                       unit: ''},
+    shadowOpacity:              {value: 0.03,                                    unit: ''},
+    intersectionTolerance:      {value: 0.0001,                                  unit: ''},
+    populateLinesDuration:      {value: 5000,                                    unit: ''},
 
     // colours
-    bg: '#eee',
-    fg: '#555',
-    fg1p5: '#999',
-    fg2: '#bbb',
-    highlight: 'rgba(0,0,0,0.1)',
-    bgSat: 60,
-    bgLight: 95,
+    bg:                         {value: '#eee',                                  unit: ''},
+    fg:                         {value: '#555',                                  unit: ''},
+    fg1p5:                      {value: '#999',                                  unit: ''},
+    fg2:                        {value: '#bbb',                                  unit: ''},
+    highlight:                  {value: 'rgba(0,0,0,0.1)',                       unit: ''},
+    bgSat:                      {value: 60,                                      unit: '%'},
+    bgLight:                    {value: 95,                                      unit: '%'},
 
     // other
-    linePreview: <LineType | null>null,
-    lines: <LineType[]>[],
-    clickedCoord: <Coord | null>null,
-    lightSource: <Coord>{y: 0, x: window.innerWidth / 2},
-    enableLogs: true,
+    linePreview:                {value: <LineType | null>null,                   unit: 'hide'},
+    lines:                      {value: <LineType[]>[],                          unit: 'hide'},
+    clickedCoord:               {value: <Coord | null>null,                      unit: 'hide'},
+    lightSource:                {value: <Coord>{y: 0, x: window.innerWidth / 2}, unit: 'hide'},
+    enableLogs:                 {value: true,                                    unit: 'hide'},
 }
 
-const units = <{[key in keyof typeof storeState]: string}>{
-    // lengths
-    appWidth: 'px',
-    appHeight: 'px',
-    paddingMed: 'px',
-    cellWidth: 'px',
-    dotSize: 'px',
-    shadowBlurRadius: 'px',
+const typeSafeSet = <T, K extends keyof T, V extends T[K]>(obj: T, key: K, value: V) => {
+    obj[key] = value
+}
 
-    // numbers
-    gridSizeW: '',
-    gridSizeH: '',
-    maxLineLength: '',
-    mouseThrottle: '',
-    minDistanceToLineForShadow: '',
-    shadowOpacity: '',
-    intersectionTolerance: '',
-    populateLinesDuration: '',
-
-    // colours
-    bg: '',
-    fg: '',
-    fg1p5: '',
-    fg2: '',
-    highlight: '',
-    bgSat: '%',
-    bgLight: '%',
-
-    // other
-    linePreview: 'hide',
-    lines: 'hide',
-    clickedCoord: 'hide',
-    lightSource: 'hide',
-    enableLogs: 'hide',
+const storeState = <{[T in keyof typeof config]: typeof config[T]['value']}>{}
+const units = <{[T in keyof typeof config]: string}>{}
+for (const keyString in config) {
+    const key = keyString as keyof typeof config
+    typeSafeSet(storeState, key, config[key]['value'])
+    typeSafeSet(units,      key, config[key]['unit'])
 }
 
 export {storeState, units}
