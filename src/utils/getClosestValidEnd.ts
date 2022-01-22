be7fc785ebedb636fe3df1f4e0a4da659d956c10
Felix Wu <felix@2simple.com>
2022-01-22 14:40:07 +0000

    Fix bug with line length 1 where created line was zero-length

--- src/utils/getClosestValidEnd.ts
+++ src/utils/getClosestValidEnd.ts
@@ -1,12 +1,13 @@
 import {Coord, LineType} from '@/types'
 import isLineTooLong from '@/utils/isLineTooLong'
 import getFourClosestCoords from '@/utils/getFourClosestCoords'
+import isSameCoordinate from "@/utils/isSameCoordinate";
 
 export default (line: LineType): Coord | null => {
     const closestCoords = getFourClosestCoords(line.end)
     const closestCoord = closestCoords.find((coord) => {
         const lineToCheck = {start: line.start, end: coord}
-        return !isLineTooLong(lineToCheck)
+        return !isLineTooLong(lineToCheck) && !isSameCoordinate(lineToCheck.start, lineToCheck.end)
     })
     if (closestCoord) {
         return closestCoord
